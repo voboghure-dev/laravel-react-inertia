24d39c9a1da816dc5db602b4b9ff367b1026f753
Tapan Kumer Das <voboghure.dev@gmail.com>
2024-04-26 19:33:49 +0600

    Create controller and routes
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,39 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::redirect( '/', '/dashboard' );
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::get( '/dashboard', function () {
+// 	return Inertia::render( 'Dashboard' );
+// } )->middleware( ['auth', 'verified'] )->name( 'dashboard' );
 
-require __DIR__.'/auth.php';
+Route::middleware( ['auth', 'verified'] )->group( function () {
+	Route::get( '/dashboard', fn() => Inertia::render( 'Dashboard' ) )->name( 'dashboard' );
+
+	Route::resource( 'project', ProjectController::class );
+	Route::resource( 'task', TaskController::class );
+	Route::resource( 'user', UserController::class );
+} );
+
+Route::middleware( 'auth' )->group( function () {
+	Route::get( '/profile', [ProfileController::class, 'edit'] )->name( 'profile.edit' );
+	Route::patch( '/profile', [ProfileController::class, 'update'] )->name( 'profile.update' );
+	Route::delete( '/profile', [ProfileController::class, 'destroy'] )->name( 'profile.destroy' );
+} );
+
+require __DIR__ . '/auth.php';
